480ab9e0b5d0c5b39f361ef07d98b799e2de8d57
Sakrymo <kr.moskal@email.com>
2021-07-14 08:06:14 +0200

    -

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
+// Hero section scaling
 const heroSection = document.getElementById('hero')
 const menuHeight = document.querySelector('#navbar').offsetHeight + document.querySelector('.navbar-gradient').offsetHeight
-
 
 heroSection.style.height = `${window.innerHeight - menuHeight}px`
 
 window.addEventListener('resize', e => {
   heroSection.style.height = `${window.innerHeight - menuHeight}px`;
 })
+
+// We create _______
+const thingsWeCreate = [
+  'Logos',
+  'Posters',
+  'Illustrations',
+  'Leaflets',
+  'Flyers',
+  'Social Media Ads',
+  'Illustrations',
+  ''
+]
